2eb4324fbc43fb6c24c3d46a59764a43764309cc
miketeow <27071053+miketeow@users.noreply.github.com>
2025-09-23 14:27:18 +0800

    customize theme toggle

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -10,7 +10,10 @@ export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <Toggle onClick={() => setTheme(theme == "dark" ? "light" : "dark")}>
+    <Toggle
+      onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
+      className="data-[state=on]:bg-transparent data-[state=on]:text-inherit"
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
